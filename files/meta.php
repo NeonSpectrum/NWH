59ddfc60283bedf28478b89aa4b4723256b07b4b
Manny Young <youngskymann@gmail.com>
2017-10-15 18:19:40 +0800

    removed cache

--- files/meta.php
+++ files/meta.php
@@ -1,3 +1,3 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Cache-control" content="public">
+<!-- <meta http-equiv="Cache-control" content="public"> -->
