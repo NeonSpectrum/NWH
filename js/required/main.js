afa7e9407dfd48050c111e1e582296548c78a22e
Manny Young <youngskymann@gmail.com>
2017-10-15 18:05:15 +0800

    fixed timeout

--- js/required/main.js
+++ js/required/main.js
@@ -62,12 +62,14 @@ function sendMsg(msg){
  }
 } */
 
-function alertNotif(type,message,reload){
+function alertNotif(type,message,reload,timeout){
 	if(type == "success")
 		type = "alert-success";
 	else if(type == "error")
 		type = "alert-danger";
-	$('#alertBox').html('<div data-notify="container" class="col-xs-11 col-sm-4 alert animated fadeInDown text-center '+type+'" role="alert" data-notify-position="top-center" style="display: inline-block; margin: 0px auto; position: fixed; z-index: 1031; top: 20px; left: 0px; right: 0px;"><span data-notify="icon"></span><span data-notify="title"></span><span data-notify="message">'+message+'</span></div>');
+	if(timeout==null)
+		timeout=2000;
+	$('#alertBox').html('<div data-notify="container" class="col-xs-11 col-sm-4 alert animated fadeInDown text-center '+type+'" role="alert" data-notify-position="top-center" style="display: inline-block; margin: 0px auto; position: fixed; z-index: 1031; top: 20px; left: 0px; right: 0px;"><span data-notify="icon"></span><span data-notify="title"></span><span data-notify="message">'+message+'</span><button type="button" aria-hidden="true" class="close" data-dismiss = "alert" style="position: absolute; right: 10px; top: 20px; margin-top: -13px; z-index: 1033;">×</button></div>');
 	$('#alertBox').fadeIn();
 	setTimeout(function(){
 		$('#alertBox').fadeOut();
@@ -77,5 +79,5 @@ function alertNotif(type,message,reload){
 			location.reload(reload);
 		else
 			location.href(reload);
-	},2000);
+	},timeout);
 }
