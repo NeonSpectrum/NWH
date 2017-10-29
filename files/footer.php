208f0aeed2dae33e6f18f00db1bbd4dc0bbbeb22
NeonSpectrum <youngskymann@gmail.com>
2017-10-30 00:38:47 +0800

    removed margin-top in footer suggested by cg

--- files/footer.php
+++ files/footer.php
@@ -6,7 +6,7 @@
 	else
 	{
 ?>
-<div style="margin-top:20px;background:rgba(142, 196, 231, 1);width:100%;height:auto;">
+<div style="background:rgba(142, 196, 231, 1);width:100%;height:auto;">
 	<div class="center-block" style="width:80%;padding-left:10%;font-size:15px">
 		<div class="footer-content">
 			<hr style="border-color:white;border-width:3px;width:10%;" align="left"/>
