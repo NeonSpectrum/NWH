ac6413a78e970cbee16817ec2e98312e941e6f49
NeonSpectrum <youngskymann@gmail.com>
2017-11-02 18:34:26 +0800

    remove comment

--- files/db.php
+++ files/db.php
@@ -6,10 +6,9 @@
 	$username = "NeonSpectrum";
 	$password = PASSWORD;
 	$database = "nwh";
-	// Create connection
+
 	$db = mysqli_connect($servername, $username, $password, $database);
 
-	// Check connection
 	if (!$db)
 	{
 		$db = mysqli_connect("localhost", "root", $password, $database);
