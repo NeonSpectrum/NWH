faa9bcc04b75c65d84401b161c8ca1b8856e3031
NeonSpectrum <youngskymann@gmail.com>
2017-11-26 23:14:29 +0800

    git update test 1

--- files/gitUpdate.php
+++ files/gitUpdate.php
@@ -1,4 +1,4 @@
 <?php
   $root = substr($_SERVER['PHP_SELF'], 0, 5) == '/nwh/' ? '/nwh/' : '/';
-  echo trim(nl2br(shell_exec("cd /var/www/html{$root} && git pull origin master 2>&1")));
+  echo shell_exec("cd /var/www/html{$root} && git pull origin master 2>&1");
 ?>
