36a9c04e68f0120e6fda474ece0890d941993fcd
NeonSpectrum <youngskymann@gmail.com>
2017-11-30 16:57:39 +0800

    test gitupdate

--- ajax/gitUpdate.php
+++ ajax/gitUpdate.php
@@ -1,4 +1,5 @@
 <?php
   $root = substr($_SERVER['PHP_SELF'], 0, 5) == '/nwh/' ? '/nwh/' : '/';
-  echo str_replace("\r\n", "\n", shell_exec("cd ".str_replace("\ajax","",__DIR__)." && git pull origin master 2>&1"));
+  // echo str_replace("\r\n", "\n", shell_exec("cd ".str_replace("\ajax","",__DIR__)." && git pull origin master 2>&1"));
+  echo "cd ".str_replace("\ajax","",__DIR__)." && git pull origin master 2>&1";
 ?>
