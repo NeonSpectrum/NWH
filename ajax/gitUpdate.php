802b5d0e0a38fbce97eb01761bb18c83e8f329e3
NeonSpectrum <youngskymann@gmail.com>
2017-11-30 17:32:27 +0800

    fix git update not working

--- ajax/gitUpdate.php
+++ ajax/gitUpdate.php
@@ -1,5 +1,3 @@
 <?php
-  $root = substr($_SERVER['PHP_SELF'], 0, 5) == '/nwh/' ? '/nwh/' : '/';
-  // echo str_replace("\r\n", "\n", shell_exec("cd ".str_replace("\ajax","",__DIR__)." && git pull origin master 2>&1"));
-  echo "cd ".str_replace("\ajax","",__DIR__)." && git pull origin master 2>&1";
+  echo str_replace("\r\n", "\n", shell_exec("export PATH=$PATH:~/git-2.9.5 && cd ".str_replace("ajax","",__DIR__)." && git pull origin master 2>&1"));
 ?>
