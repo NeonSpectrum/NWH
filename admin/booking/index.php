5d61a4e71976cfc61117eea6be2e275bdcb4c3ce
NeonSpectrum <youngskymann@gmail.com>
2017-12-02 01:12:25 +0800

    make table responsive

--- admin/booking/index.php
+++ admin/booking/index.php
@@ -11,38 +11,40 @@
   <div class="content-wrapper content-wrapper--with-bg">
     <h1 class="page-title">Booking</h1>
     <div class="well">
-      <table id="tblReservation" class="table table-striped table-bordered table-hover">
-        <thead>
-          <?php
-            $query = "SHOW COLUMNS FROM booking";
-            $result = mysqli_query($db,$query);
-            while ($row = mysqli_fetch_assoc($result))
-            {
-              echo "<th>{$row['Field']}</th>\n";
-            }
-          ?>	
-          <th>Report</th>
-        </thead>
-        <tbody>
-          <?php
-            $query = "SELECT * FROM booking";
-            $result = mysqli_query($db,$query) or die(mysql_error());
-            while ($row = mysqli_fetch_assoc($result))
-            {
-              echo "<tr>";
-              echo "<td>{$row['BookingID']}</td>";
-              echo "<td>{$row['EmailAddress']}</td>";
-              echo "<td>{$row['RoomID']}</td>";
-              echo "<td>{$row['CheckInDate']}</td>";
-              echo "<td>{$row['CheckOutDate']}</td>";
-              echo "<td>{$row['Adults']}</td>";
-              echo "<td>{$row['Children']}</td>";
-              echo "<td><a href='{$root}files/generateReservationConfirmation/?BookingID={$row['BookingID']}'>View Reservation</a></td>";
-              echo "</tr>";
-            }
-          ?>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table id="tblReservation" class="table table-striped table-bordered table-hover">
+          <thead>
+            <?php
+              $query = "SHOW COLUMNS FROM booking";
+              $result = mysqli_query($db,$query);
+              while ($row = mysqli_fetch_assoc($result))
+              {
+                echo "<th>{$row['Field']}</th>\n";
+              }
+            ?>	
+            <th>Report</th>
+          </thead>
+          <tbody>
+            <?php
+              $query = "SELECT * FROM booking";
+              $result = mysqli_query($db,$query) or die(mysql_error());
+              while ($row = mysqli_fetch_assoc($result))
+              {
+                echo "<tr>";
+                echo "<td>{$row['BookingID']}</td>";
+                echo "<td>{$row['EmailAddress']}</td>";
+                echo "<td>{$row['RoomID']}</td>";
+                echo "<td>{$row['CheckInDate']}</td>";
+                echo "<td>{$row['CheckOutDate']}</td>";
+                echo "<td>{$row['Adults']}</td>";
+                echo "<td>{$row['Children']}</td>";
+                echo "<td><a href='{$root}files/generateReservationConfirmation/?BookingID={$row['BookingID']}'>View Reservation</a></td>";
+                echo "</tr>";
+              }
+            ?>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </main>
