a4a43b059985fa3f2fd0f15fc7965b4912f4acc5
NeonSpectrum <youngskymann@gmail.com>
2017-12-13 22:17:08 +0800

    update db

--- cp018101_nwh.sql
+++ cp018101_nwh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2017 at 04:08 AM
+-- Generation Time: Dec 13, 2017 at 03:14 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -36,6 +36,7 @@ CREATE TABLE `account` (
   `FirstName` text NOT NULL,
   `LastName` text NOT NULL,
   `ContactNumber` varchar(20) NOT NULL,
+  `BirthDate` date NOT NULL,
   `DateRegistered` date NOT NULL,
   `SessionID` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -44,12 +45,12 @@ CREATE TABLE `account` (
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`EmailAddress`, `Password`, `AccountType`, `ProfilePicture`, `FirstName`, `LastName`, `ContactNumber`, `DateRegistered`, `SessionID`) VALUES
-('beajewelcvines@gmail.com', '$2y$10$VFNjVijVyv73K1tq8Fs5.uds4JwDpaRvMh1yy2BWKETxlj9PQ7Aw2', 'Admin', 'default.png', 'Bea Jewel', 'Vines', '', '2017-12-04', 'ioccmabmgrhh7j9700gbi3ni0h'),
-('gunorica@yahoo.com', '$2y$10$eA1bkvZnfI4dZs/BTCFIgeNT48y/GCNLjL8tJiHBwiEndi8LPzx66', 'Admin', 'default.png', 'Rica', 'Guno', '', '2017-10-29', 'k9mnvrc9su4m29e2fahve2uglm'),
-('jasonallego08@gmail.com', '$2y$10$FmDkJc0xJwpN6DMpnR16U.RjGsyLODqVUJvfUorWwlCdsipdY/e1C', 'Admin', 'JasonAllego.jpg', 'Jason', 'Allego', '', '2017-11-20', 'cuv527khttgpq9r22ufkv30ru0'),
-('katebolanos2@gmail.com', '$2y$10$SqvhZvQpQCMFdLFnIPVbd.Z7MQuhin0OlJgkf2JgUwqcu0/Wr6wPa', 'Admin', 'KateBolanos.png', 'Kate', 'Bolanos', '', '2017-11-29', '581tl75evccq32dpuia8e263u7'),
-('youngskymann@gmail.com', '$2y$10$luX9F27Y3LawOTgXiTXZh.VBNdGawSHEIdvpuE2wwz9y3rILcBZlC', 'Owner', 'MannyYoung.png', 'Manny', 'Young', '', '2017-11-25', '4clbb8f9f9kmreegut891vvovt');
+INSERT INTO `account` (`EmailAddress`, `Password`, `AccountType`, `ProfilePicture`, `FirstName`, `LastName`, `ContactNumber`, `BirthDate`, `DateRegistered`, `SessionID`) VALUES
+('beajewelcvines@gmail.com', '$2y$10$VFNjVijVyv73K1tq8Fs5.uds4JwDpaRvMh1yy2BWKETxlj9PQ7Aw2', 'Admin', 'default.png', 'Bea Jewel', 'Vines', '123', '2017-12-13', '2017-12-04', 'ioccmabmgrhh7j9700gbi3ni0h'),
+('gunorica@yahoo.com', '$2y$10$eA1bkvZnfI4dZs/BTCFIgeNT48y/GCNLjL8tJiHBwiEndi8LPzx66', 'Admin', 'default.png', 'Rica', 'Guno', '123', '2017-12-13', '2017-10-29', 'k9mnvrc9su4m29e2fahve2uglm'),
+('jasonallego08@gmail.com', '$2y$10$FmDkJc0xJwpN6DMpnR16U.RjGsyLODqVUJvfUorWwlCdsipdY/e1C', 'Admin', 'JasonAllego.jpg', 'Jason', 'Allego', '123', '2017-12-13', '2017-11-20', 'cuv527khttgpq9r22ufkv30ru0'),
+('katebolanos2@gmail.com', '$2y$10$SqvhZvQpQCMFdLFnIPVbd.Z7MQuhin0OlJgkf2JgUwqcu0/Wr6wPa', 'Admin', 'KateBolanos.png', 'Kate', 'Bolanos', '123', '2017-12-13', '2017-11-29', '581tl75evccq32dpuia8e263u7'),
+('youngskymann@gmail.com', '$2y$10$luX9F27Y3LawOTgXiTXZh.VBNdGawSHEIdvpuE2wwz9y3rILcBZlC', 'Owner', 'MannyYoung.png', 'Manny', 'Young', '123', '2017-12-13', '2017-11-25', 'd1llb8t9hu2u3s6pa2m5cpsp06');
 
 -- --------------------------------------------------------
 
@@ -69,15 +70,6 @@ CREATE TABLE `booking` (
   `TotalAmount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `booking`
---
-
-INSERT INTO `booking` (`BookingID`, `EmailAddress`, `RoomID`, `CheckInDate`, `CheckOutDate`, `Adults`, `Children`, `AmountPaid`, `TotalAmount`) VALUES
-(1, 'youngskymann@gmail.com', 206, '2017-12-07', '2017-12-07', 4, 1, 0, 3850),
-(2, 'youngskymann@gmail.com', 108, '2017-12-09', '2017-12-09', 1, 0, 0, 3500),
-(4, 'youngskymann@gmail.com', 211, '2017-12-13', '2017-12-13', 1, 0, 0, 3500);
-
 -- --------------------------------------------------------
 
 --
@@ -92,18 +84,6 @@ CREATE TABLE `reservation` (
   `CheckOut` datetime DEFAULT NULL,
   `Price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`ReservationID`, `BookingID`, `WalkInID`, `CheckIn`, `CheckOut`, `Price`) VALUES
-(1, NULL, 1, '2017-12-06 08:22:24', '2017-12-06 08:22:34', NULL),
-(2, NULL, 2, '2017-12-06 08:28:08', '2017-12-06 08:28:37', NULL),
-(3, NULL, 3, '2017-12-06 08:37:15', '2017-12-06 08:37:22', NULL),
-(4, NULL, 4, '2017-12-06 10:26:06', '2017-12-06 10:26:10', NULL),
-(5, NULL, 5, '2017-12-06 11:28:24', '2017-12-06 11:28:28', NULL),
-(6, NULL, 6, '2017-12-06 11:28:59', '2017-12-06 15:35:08', NULL);
 
 -- --------------------------------------------------------
 
@@ -122,27 +102,27 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`RoomID`, `RoomTypeID`, `Status`) VALUES
-(101, 1, 'Occupied'),
+(101, 1, 'Enabled'),
 (102, 1, 'Enabled'),
 (103, 1, 'Enabled'),
 (104, 5, 'Enabled'),
 (105, 5, 'Enabled'),
 (106, 5, 'Enabled'),
 (107, 5, 'Enabled'),
-(108, 6, 'Occupied'),
-(109, 6, 'Occupied'),
+(108, 6, 'Enabled'),
+(109, 6, 'Enabled'),
 (110, 5, 'Enabled'),
 (111, 5, 'Enabled'),
 (112, 5, 'Enabled'),
 (201, 1, 'Enabled'),
 (202, 1, 'Enabled'),
-(203, 2, 'Occupied'),
+(203, 2, 'Enabled'),
 (204, 2, 'Enabled'),
-(205, 4, 'Occupied'),
-(206, 3, 'Occupied'),
-(207, 4, 'Occupied'),
-(208, 3, 'Occupied'),
-(209, 2, 'Occupied'),
+(205, 4, 'Enabled'),
+(206, 3, 'Enabled'),
+(207, 4, 'Enabled'),
+(208, 3, 'Enabled'),
+(209, 2, 'Enabled'),
 (210, 2, 'Enabled'),
 (211, 1, 'Enabled'),
 (212, 1, 'Enabled');
@@ -194,18 +174,6 @@ CREATE TABLE `walk_in` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `walk_in`
---
-
-INSERT INTO `walk_in` (`WalkInID`, `EmailAddress`, `RoomID`, `CheckInDate`, `CheckOutDate`, `Adults`, `Children`, `AmountPaid`, `TotalAmount`) VALUES
-(1, 'youngskymann@gmail.com', 201, '2017-12-06', '2017-12-06', 1, 0, 0, 2250),
-(2, 'youngskymann@gmail.com', 102, '2017-12-06', '2017-12-20', 1, 0, 0, 2250),
-(3, 'youngskymann@gmail.com', 210, '2017-12-06', '2017-12-07', 4, 0, 0, 2900),
-(4, 'youngskymann@gmail.com', 202, '2017-12-06', '2017-12-06', 1, 0, 0, 2250),
-(5, 'youngskymann@gmail.com', 104, '2017-12-06', '2017-12-06', 4, 0, 0, 1500),
-(6, 'beajewelcvines@gmail.com', 202, '2017-12-06', '2017-12-06', 1, 0, 0, 2250);
-
---
 -- Indexes for dumped tables
 --
 
@@ -220,7 +188,8 @@ ALTER TABLE `account`
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`BookingID`,`EmailAddress`,`RoomID`),
-  ADD KEY `RoomID` (`RoomID`);
+  ADD KEY `RoomID` (`RoomID`),
+  ADD KEY `EmailAddress` (`EmailAddress`);
 
 --
 -- Indexes for table `reservation`
@@ -247,7 +216,9 @@ ALTER TABLE `room_type`
 -- Indexes for table `walk_in`
 --
 ALTER TABLE `walk_in`
-  ADD PRIMARY KEY (`WalkInID`);
+  ADD PRIMARY KEY (`WalkInID`),
+  ADD KEY `RoomID` (`RoomID`),
+  ADD KEY `EmailAddress` (`EmailAddress`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -257,13 +228,13 @@ ALTER TABLE `walk_in`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `BookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `BookingID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ReservationID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `room_type`
@@ -275,7 +246,7 @@ ALTER TABLE `room_type`
 -- AUTO_INCREMENT for table `walk_in`
 --
 ALTER TABLE `walk_in`
-  MODIFY `WalkInID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `WalkInID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -285,7 +256,8 @@ ALTER TABLE `walk_in`
 -- Constraints for table `booking`
 --
 ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`);
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`EmailAddress`) REFERENCES `account` (`EmailAddress`);
 
 --
 -- Constraints for table `reservation`
@@ -299,6 +271,13 @@ ALTER TABLE `reservation`
 --
 ALTER TABLE `room`
   ADD CONSTRAINT `room_ibfk_1` FOREIGN KEY (`RoomTypeID`) REFERENCES `room_type` (`RoomTypeID`);
+
+--
+-- Constraints for table `walk_in`
+--
+ALTER TABLE `walk_in`
+  ADD CONSTRAINT `walk_in_ibfk_1` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`),
+  ADD CONSTRAINT `walk_in_ibfk_2` FOREIGN KEY (`EmailAddress`) REFERENCES `account` (`EmailAddress`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
