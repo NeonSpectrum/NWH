e762f52f2aada6d1186b3f3086db54721551a691
NeonSpectrum <youngskymann@gmail.com>
2017-12-23 20:57:27 +0800

    added session_start at functions

--- files/functions.php
+++ files/functions.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once __DIR__ . '/../assets/PHPMailer/src/Exception.php';
 require_once __DIR__ . '/../assets/PHPMailer/src/PHPMailer.php';
 require_once __DIR__ . '/../assets/PHPMailer/src/SMTP.php';
