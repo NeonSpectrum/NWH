b039b14ca84cc87d4cac85c8dac6e43313a52aa6
NeonSpectrum <youngskymann@gmail.com>
2017-12-23 19:54:50 +0800

    fix double quotes git update

--- ajax/gitUpdate.php
+++ ajax/gitUpdate.php
@@ -5,5 +5,5 @@ if ($_SERVER['SERVER_NAME'] == "localhost") {
   return;
 }
 createLog("git|update");
-echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec("export PATH=$PATH:~/git-2.9.5 && cd " . str_replace("ajax", "", __DIR__) . " && git pull origin master 2>&1"))));
+echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec('export PATH=$PATH:~/git-2.9.5 && cd ' . str_replace("ajax", "", __DIR__) . ' && git pull origin master 2>&1'))));
 ?>
