0f97e573146fdb9dd3eefca95a74634076e1d449
NeonSpectrum <youngskymann@gmail.com>
2017-12-24 18:45:20 +0800

    edited log on git update

--- ajax/gitUpdate.php
+++ ajax/gitUpdate.php
@@ -4,7 +4,10 @@ require_once "../files/db.php";
 if ($_SERVER['SERVER_NAME'] != "localhost") {
   echo false;
 } else {
-  createLog("git|update");
-  echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec('export PATH=$PATH:~/git-2.9.5 && cd ' . str_replace("ajax", "", __DIR__) . ' && git pull origin master 2>&1'))));
+  $output = trim(preg_replace('/\s+/', ' ', nl2br(shell_exec('export PATH=$PATH:~/git-2.9.5 && cd ' . str_replace("ajax", "", __DIR__) . ' && git pull origin master 2>&1'))));
+  if (strpos($output, "files changed")) {
+    createLog("git|update|" . substr(str_replace("<br>", "", substr($output, strrpos($output, "<br>", "-5"), "-1")), 0, strpos(str_replace("<br>", "", substr($output, strrpos($output, "<br>", "-5"), "-1")), ",")););
+  }
+  echo $output;
 }
 ?>
