5f4cb1951bb79393dac16c4af8f00540f0648bac
NeonSpectrum <youngskymann@gmail.com>
2017-12-31 17:23:00 +0800

    wow debug.php

--- debug.php
+++ debug.php
@@ -1,36 +1,215 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec('export PATH=$$PATH:~/git-2.9.5 && ' . $_POST['command']))));
+  $path = strtolower($_SERVER['SERVER_NAME']) != "localhost" ? 'export PATH=$PATH:~/git-2.9.5 && ' : '';
+  echo trim(preg_replace('/\s+/', ' ', nl2br(shell_exec($path . $_POST['command']))));
   return;
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>Terminal</title>
+<style>
+
+@font-face {
+  font-family: 'Nunito';
+  font-style: normal;
+  font-weight: 400;
+  src: url("assets/css/fonts/SourceCodePro-Regular.ttf");
+}
+
+html, body {
+  height: 100%;
+  overflow: hidden;
+  margin:0;
+}
+
+body {
+  background: #3a7bd5;
+  background-image: -webkit-radial-gradient(top, circle cover, #00d2ff 0%, #3a7bd5 80%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+textarea, input, button {
+  outline: none;
+}
+
+.container{
+  height: 100%;
+  width: 100%;
+}
+
+.window {
+  animation: fade 1s ease-in-out;
+  width: 100%;
+  height: 100%;
+}
+.window .handle {
+  height: 22px;
+  background: linear-gradient(0deg, #d8d8d8, #ececec);
+  border-top: 1px solid white;
+  border-bottom: 1px solid #b3b3b3;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  color: rgba(0, 0, 0, 0.7);
+  font-family: Helvetica, sans-serif;
+  font-size: 13px;
+  line-height: 22px;
+  text-align: center;
+}
+.window .buttons {
+  position: absolute;
+  float: left;
+  margin: 0 8px;
+}
+.window .buttons .close {
+  background-color: #ff6159;
+}
+.window .buttons .minimize {
+  background-color: #ffbf2f;
+}
+.window .buttons .maximize {
+  background-color: #25cc3e;
+}
+.window .terminal {
+  padding: 4px;
+  background-color: black;
+  opacity: 0.7;
+  height: 97%;
+  color: white;
+  font-family: 'Source Code Pro', monospace;
+  font-weight: 200;
+  font-size: 14px;
+  white-space: pre-wrap;
+  white-space: -moz-pre-wrap;
+  white-space: -pre-wrap;
+  white-space: -o-pre-wrap;
+  word-wrap: break-word;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  overflow-y: auto;
+}
+.window .terminal::after {
+  content: "|";
+  animation: blink 2s steps(1) infinite;
+}
+
+.prompt {
+  color: #bde371;
+}
+
+.path {
+  color: #5ed7ff;
+}
+
+@keyframes blink {
+  50% {
+    color: transparent;
+  }
+}
+@keyframes fade {
+  from {
+    opacity: 0
+  }
+  100% {
+    opacity: 1
+  }
+}
+</style>
 </head>
 <body>
-  <form id="frmCommand">
-    <input type="text" name="command" autofocus autocomplete="off">
-  </form>
-  <br/>
-  <div id="message"></div>
-  <script src="assets/js/required/1_jquery.min.js"></script>
-  <script>
-    $('#frmCommand').submit(function(e){
+<div class="container">
+  <div class="window">
+    <div class="handle">
+      <span class="title"></span>
+    </div>
+    <div class="terminal"></div>
+  </div>
+</div>
+<script src='assets/js/required/1_jquery.min.js'></script>
+<script>
+$(document).ready(function() {
+  "use strict";
+
+  function clear() {
+    terminal.text("");
+  }
+
+  var title = $(".title");
+  var terminal = $(".terminal");
+  var prompt = "➜";
+  var path = "~";
+  var command = "";
+
+  function displayPrompt() {
+    terminal.append("<span class=\"prompt\">" + prompt + "</span> ");
+    terminal.append("<span class=\"path\">" + path + "</span> ");
+  }
+  function erase(n) {
+    command = command.slice(0, -n);
+    terminal.html(terminal.html().slice(0, -n));
+  }
+  function appendCommand(str) {
+    terminal.append(str);
+    command += str;
+  }
+  $(document).keydown(function(e) {
+    e = e || window.event;
+    var keyCode = typeof e.which === "number" ? e.which : e.keyCode;
+
+    if (keyCode === 8 && e.target.tagName !== "INPUT" && e.target.tagName !== "TEXTAREA") {
       e.preventDefault();
-      $.ajax({
-        context: this,
-        type: 'POST',
-        url: 'debug.php',
-        data: $(this).serialize(),
-        success: function(response) {
-          $('#message').html(response);
-          $(this).trigger("reset");
+      if (command !== "") {
+        erase(1);
+      }
+    }
+  });
+  $(document).keypress(function(e) {
+    e = e || window.event;
+    var keyCode = typeof e.which === "number" ? e.which : e.keyCode;
+
+    switch (keyCode) {
+      case 13:
+      {
+        terminal.append("\n");
+        if(command.toLowerCase()=="clear"){
+          terminal.text("");
+          displayPrompt();
+          command = "";
+        } else {
+          $.ajax({
+            context: this,
+            type: 'POST',
+            data: "command="+command,
+            success: function(response) {
+              if(response){
+                terminal.append(response);
+              } else {
+                terminal.append("sh: command not found: " + command.replace(/<\/?[^>]+(>|$)/g, "") + "\n");
+              }
+              displayPrompt();
+              command = "";
+              $(".window .terminal").scrollTop($('.window .terminal')[0].scrollHeight);
+            }
+          });
         }
-      });
-    });
-  </script>
+        break;
+      }
+      default:
+      {
+        appendCommand(String.fromCharCode(keyCode));
+      }
+    }
+  });
+
+  title.text("1. NeonSpectrum@<?php echo $_SERVER['SERVER_NAME']; ?>: ~ (sh)");
+  terminal.append("Current Time: " + new Date() + "\n"); displayPrompt();
+});
+</script>
 </body>
 </html>
