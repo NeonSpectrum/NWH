983e01b9fab0bd946730dfc941107cb62dd6cf41
NeonSpectrum <youngskymann@gmail.com>
2018-01-06 15:59:14 +0800

    added reservation at redirect login

--- account/login.php
+++ account/login.php
@@ -37,9 +37,107 @@ require_once "../header.php";
           </div>
           <div class="form-group">
             <button id="btnLogin" type="submit" class="btn btn-primary btn-block">Sign in</button>
+            <button class="btn btn-default btn-block" type="button" data-toggle="modal" data-target="#modalRegistration">Register</button>
           </div>
         </form>
       </div>
+    </div>
+  </div>
+</div>
+<div id="modalRegistration" class="modal fade" role="dialog" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title text-center">Registration</h4>
+      </div>
+      <form id="frmRegister" data-toggle="validator">
+        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token ?>"/>
+        <div class="modal-body">
+          <div class="lblDisplayError">
+            <!-- errors will be shown here ! -->
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <b>Note:</b> You must verify the email address to register your account.
+              <br/>
+              <br/>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group has-feedback">
+                <label>First Name<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-user-o"></span></span>
+                  <input type="text" name="txtFirstName" id="txtFirstName" class="form-control" pattern="[a-zA-Z ]*$" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+              <div class="form-group has-feedback">
+                <label>Last Name<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-user-o"></span></span>
+                  <input type="text" name="txtLastName" id="txtLastName" class="form-control" pattern="[a-zA-Z ]*$" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+              <div class="form-group has-feedback">
+                <label>Birth Date<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-calendar"></span></span>
+                  <input type="text" name="txtBirthDate" id="txtBirthDate" class="form-control birthDate" placeholder="mm/dd/yyyy" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+              <div class="form-group has-feedback">
+                <label>Contact Number<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-mobile fa-lg"></span></span>
+                  <input type="text" name="txtContactNumber" id="txtContactNumber" class="form-control" pattern="[0-9]*$" onkeypress="return disableKey(event,'letter');" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group has-feedback">
+                <label>Email Address<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-envelope-o"></span></span>
+                  <input type="email" name="txtEmail" id="txtEmail" class="form-control" data-error="<?php echo REGISTER_EMAIL_ERROR; ?>" data-remote="<?php echo $root; ?>account?mode=checkEmail" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+              <div class="form-group has-feedback">
+                <label>Password<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-key"></span></span>
+                  <input type="password" name="txtPassword" id="txtPassword" class="form-control" minlength="8" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+              <div class="form-group has-feedback">
+                <label>Verify Password<sup>*</sup></label>
+                <div class="input-group">
+                  <span class="input-group-addon"><span class="fa fa-key"></span></span>
+                  <input type="password" name="txtRetypePassword" id="txtRetypePassword" class="form-control" minlength="8" data-match="#txtPassword" data-match-error="Whoops, these don't match" required autocomplete="off">
+                </div>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                <div class="help-block with-errors"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <div style="margin-bottom: 10px" class="g-recaptcha pull-left" data-callback="recaptchaCallback" data-expired-callback="expiredCallback" data-sitekey="6Ler0DUUAAAAAK0dRPfLXX4i3HXRKZCmvdLzyRDp"></div>
+          <button id="btnRegister" type="submit" class="btn btn-info" disabled>Register</button>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
