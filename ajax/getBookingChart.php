ac8ddaead453827df1dcf98768347c0563844b44
NeonSpectrum <youngskymann@gmail.com>
2018-01-06 21:25:33 +0800

    optimize code on booking chart

--- ajax/getBookingChart.php
+++ ajax/getBookingChart.php
@@ -2,16 +2,12 @@
 require_once '../files/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  $result   = $db->query("SELECT CheckInDate, CheckOutDate, COUNT(*) AS CountUser FROM `booking` GROUP BY CheckInDate,CheckOutDate ORDER BY CheckInDate DESC");
-  $dates    = $arr    = [];
-  $dates[0] = date("Y-m-d", strtotime('now') - 86400 * 1);
-  $dates[1] = date("Y-m-d", strtotime('now') - 86400 * 2);
-  $dates[2] = date("Y-m-d", strtotime('now') - 86400 * 3);
-  $dates[3] = date("Y-m-d", strtotime('now') - 86400 * 4);
-  $dates[4] = date("Y-m-d", strtotime('now') - 86400 * 5);
-  $dates[5] = date("Y-m-d", strtotime('now') - 86400 * 6);
-  $dates[6] = date("Y-m-d", strtotime('now') - 86400 * 7);
-  $count    = array_fill(0, 7, 0);
+  $result = $db->query("SELECT CheckInDate, CheckOutDate, COUNT(*) AS CountUser FROM `booking` GROUP BY CheckInDate,CheckOutDate ORDER BY CheckInDate DESC");
+  $dates  = $arr  = [];
+  for ($i = 0; $i < 7; $i++) {
+    $dates[$i] = date("Y-m-d", strtotime('now') - 86400 * ($i + 1));
+  }
+  $count = array_fill(0, 7, 0);
   while ($row = $result->fetch_assoc()) {
     $rowDate = $system->getDatesFromRange($row['CheckInDate'], $row['CheckOutDate']);
     foreach ($rowDate as $key => $value) {
