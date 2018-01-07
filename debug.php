39ee1907ca95d1b1a3e8cf216111b83c435896b8
NeonSpectrum <youngskymann@gmail.com>
2018-01-07 23:02:26 +0800

    require autoload

--- debug.php
+++ debug.php
@@ -1,5 +1,5 @@
 <?php
-define("PASSWORD", "yuanhua");
+require_once "files/autoload.php";
 if (isset($_POST['command']) && $_POST['password'] === PASSWORD) {
   $command = filter_var($_POST['command'], FILTER_SANITIZE_STRING);
   $path    = strtolower($_SERVER['SERVER_NAME']) != "localhost" ? 'export PATH=$PATH:~/git-2.9.5 && ' : '';
