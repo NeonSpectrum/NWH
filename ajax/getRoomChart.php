a5706b0cbc616a76524ecc2bd8bcfaf12cb4c66f
NeonSpectrum <youngskymann@gmail.com>
2018-01-07 18:32:57 +0800

    make dates fixed last 7 days visitorchart

--- ajax/getRoomChart.php
+++ ajax/getRoomChart.php
@@ -2,12 +2,19 @@
 require_once '../files/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  $result    = $db->query("SELECT RoomID, Count(*) As CountUsed FROM booking_room GROUP BY RoomID");
-  $roomIDs   = [];
-  $countUsed = [];
+  $roomIDs = [];
+  $result  = $db->query("SELECT * FROM room");
   while ($row = $result->fetch_assoc()) {
-    $roomIDs[]   = $row['RoomID'];
-    $countUsed[] = $row['CountUsed'];
+    $roomIDs[] = $row['RoomID'];
+  }
+  $result    = $db->query("SELECT RoomID, Count(*) As CountUsed FROM booking_room GROUP BY RoomID");
+  $countUsed = array_fill(0, count($roomIDs), 0);
+  while ($row = $result->fetch_assoc()) {
+    for ($i = 0; $i < count($roomIDs); $i++) {
+      if ($row['RoomID'] == $roomIDs[$i]) {
+        $countUsed[$i] = $row['CountUsed'];
+      }
+    }
   }
   $arr    = [];
   $arr[0] = $roomIDs;
--- ajax/getVisitorChart.php
+++ ajax/getVisitorChart.php
@@ -4,14 +4,20 @@ require_once '../files/autoload.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
   $result = $db->query("SELECT * FROM `visitor-count` ORDER BY Date DESC LIMIT 7");
   $dates  = [];
-  $count  = [];
+  for ($i = 0; $i < 7; $i++) {
+    $dates[$i] = date("Y-m-d", strtotime('now') - 86400 * ($i + 1));
+  }
+  $count = array_fill(0, count($dates), 0);
   while ($row = $result->fetch_assoc()) {
-    $roomIDs[]   = $row['Date'];
-    $countUsed[] = $row['Count'];
+    for ($i = 0; $i < count($dates); $i++) {
+      if ($dates[$i] == $row['Date']) {
+        $count[$i] = $row['Count'];
+      }
+    }
   }
   $arr    = [];
-  $arr[0] = array_reverse($roomIDs);
-  $arr[1] = array_reverse($countUsed);
+  $arr[0] = array_reverse($dates);
+  $arr[1] = array_reverse($count);
   echo json_encode($arr);
 }
 ?>
