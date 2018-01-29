b867e566c5f827f8e8db82c171bb77a080328897
NeonSpectrum <youngskymann@gmail.com>
2018-01-29 23:09:27 +0800

    add missing file

---
+++ ajax/addPayment.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../files/autoload.php';
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" && $system->validateToken($_POST['csrf_token'])) {
+  $bookingID = $system->filter_input($_POST['txtBookingID']);
+  $payment   = $system->filter_input($_POST['txtPayment']);
+  if ($_POST['type'] == "booking") {
+    $db->query("UPDATE booking SET AmountPaid = AmountPaid + $payment WHERE BookingID = $bookingID");
+    if ($db->affected_rows > 0) {
+      $system->log("insert|booking.payment|$bookingID|$payment");
+    }
+  } else if ($_POST['type'] == "check") {
+    $db->query("UPDATE booking_check SET ExtraCharges = ExtraCharges + $payment WHERE BookingID = $bookingID");
+    if ($db->affected_rows > 0) {
+      $system->log("insert|booking.payment|$bookingID|$payment");
+    }
+  }
+} else if (!$system->validateToken($_POST['csrf_token'])) {
+  echo INVALID_TOKEN;
+}
+?>
