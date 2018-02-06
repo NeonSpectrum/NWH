287cdf1de1abffbbaafc9fc7eec16e1d9f198cf5
NeonSpectrum <youngskymann@gmail.com>
2018-02-06 21:59:31 +0800

    opps

--- live.js
+++ live.js
@@ -15,7 +15,6 @@ var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'))
 var io = require('socket.io').listen(http.createServer(app).listen(port = 8755, function() {
   log("Server started at port " + port)
 }))
-console.log(config.system.database_url)
 var db = mysql.createConnection({
   host: config.system.database_url,
   user: "cp018101",
