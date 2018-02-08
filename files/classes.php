5ac93195a618248c443dc6530310aefc03fec5ef
NeonSpectrum <youngskymann@gmail.com>
2018-02-08 11:27:46 +0800

    opps forgot

--- files/classes.php
+++ files/classes.php
@@ -904,7 +904,7 @@ class System {
 
   public function __construct() {
     global $db;
-    if (isset($_SESSION['account'])) {
+    if (isset($_SESSION['account']) && !$db->connect_error) {
       $this->email          = $this->decrypt($_SESSION['account']);
       $result               = $db->query("SELECT * FROM account WHERE EmailAddress='{$this->email}'");
       $row                  = $result->fetch_assoc();
