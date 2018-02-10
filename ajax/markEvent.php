e0c9ec147034b8f496744958daeff61894179cff
NeonSpectrum <youngskymann@gmail.com>
2018-02-10 17:34:36 +0800

    opps again

--- ajax/markEvent.php
+++ ajax/markEvent.php
@@ -3,7 +3,7 @@ require_once '../files/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $system->validateToken($_POST['csrf_token'])) {
   $type  = $system->filter_input($_POST['type']);
-  $dates = explode(" - ", $system->filter_input($_POST['type']));
+  $dates = explode(" - ", $system->filter_input($_POST['date']));
   echo $system->markEvent($type, $dates[0], $dates[1]);
 } else if (!$system->validateToken($_POST['csrf_token'])) {
   echo INVALID_TOKEN;
