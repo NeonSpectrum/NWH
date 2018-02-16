d8a14de2a0534cff8edd6fcd6b42a685fe7c1c6c
NeonSpectrum <youngskymann@gmail.com>
2018-02-16 21:48:52 +0800

    fix bug

--- admin/index.php
+++ admin/index.php
@@ -74,8 +74,9 @@ for ($i = 1; $row = $result->fetch_assoc(); $i++) {
           <div class="panel-heading">Current Rating</div>
           <div class="panel-body">
 <?php
-$row = $db->query("SELECT COUNT(Star) AS Number, SUM(Star) as Total FROM feedback")->fetch_assoc();
-echo $row['Number'] == null ? "N/A" : number_format($row['Total'] / $row['Number'], 1, ".", ",");
+$result = $db->query("SELECT COUNT(Star) AS Number, SUM(Star) as Total FROM feedback");
+$row    = $result->fetch_assoc();
+echo $result->num_rows == 0 ? "N/A" : number_format($row['Total'] / $row['Number'], 1, ".", ",");
 ?>
           </div>
         </div>
