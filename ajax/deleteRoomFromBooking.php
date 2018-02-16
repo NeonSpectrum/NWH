e023bb13a123406ea073ab408cdbc895f307101e
NeonSpectrum <youngskymann@gmail.com>
2018-02-16 11:28:59 +0800

    opps agian

--- ajax/deleteRoomFromBooking.php
+++ ajax/deleteRoomFromBooking.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && $system->validateToken($_POST['csrf_
   if ($db->affected_rows > 0) {
     $result       = $db->query("SELECT * FROM booking WHERE BookingID=$bookingID");
     $row          = $result->fetch_assoc();
-    $numberOfDays = count($system->getDatesFromRange($row['CheckInDate'], date("m/d/Y", strtotime($row['CheckOutDate']) - 86400)));
+    $numberOfDays = count($system->getDatesFromRange($row['CheckInDate'], $row['CheckOutDate'])) - 1;
     $price        = $room->getRoomPrice($room->getRoomType($roomID)) * $numberOfDays;
     $db->query("UPDATE booking_transaction SET TotalAmount = TotalAmount - $price WHERE BookingID=$bookingID");
     $system->log("delete|booking.room|{$system->formatBookingID($bookingID)}|$roomID");
